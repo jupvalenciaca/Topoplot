8d8299f6013e0add21fcaeebe1f5a46271ac1657
jupvalenciaca <40280327+jupvalenciaca@users.noreply.github.com>
2018-06-21 15:38:18 -0500

    creacion de las mallas y nuevo config
    se crean las mallas para los electrodos de la muestra y para los del "NY head model"
    Además se organiza para la nueva version del archivo config

--- topoplot.py
+++ topoplot.py
@@ -1,10 +1,12 @@
-import pyedflib
 import numpy as np
+import argparse
+import pyedflib
+import scipy.io
+from scipy.spatial import Delaunay
+from scipy.interpolate import griddata
+from scipy import interpolate
 import matplotlib.pyplot as plt
 from mne.viz import plot_topomap
-import scipy.io
-from scipy.interpolate import griddata
-import argparse
 
 class edf_topoplot(object):
 
@@ -19,11 +21,11 @@ class edf_topoplot(object):
 		parsedargs = parser.parse_args()
 		arc = parsedargs.archivo
 		output = parsedargs.figure
-		configg  = parsedargs.config
-		return arc, output, configg
+		config  = parsedargs.config
+		return arc, output, config
 
 	#lee el archivo .edf y extrae la informacion principal
-	def read_edf(self,archivo):
+	def read_edf(self,arc):
 
 		edf = pyedflib.EdfReader(arc)#archivo edf
 		channels_labels = edf.getSignalLabels() #etiquetas de los canales	
@@ -32,56 +34,56 @@ class edf_topoplot(object):
 		signal = np.zeros((nch,nsig))
 		for x in range(nch):
 			signal[x,:] = edf.readSignal(x)	
-		return nch,channels_labels, signal
+		return signal,nch,channels_labels
 
 	#calcula la potencia y las posiciones de los electrodos
-	def cal_topoplot(self,nch,configg,channels_labels,signal):
-
+	def calc_power(self,signal):
 		magnitud = signal**2	#magnitud
 		pot_signal = np.mean(magnitud,axis=1) #potencia de la senal
+		# print('vector de potencias')
+		# print(pot_signal)
+		return pot_signal
 
-		configgg = __import__(configg)
-		elec = configgg.elec
-		lab_ref_list = elec.keys()	#lista etiquetas de referencia
+	def calc_positions(self,config,channels_labels,nch,pot_signal):
+		config_file = __import__(config)
+		elec = config_file.elec 		#coordenadas de los electrodos de referencia
+		lab_ref_list = config_file.labels	#lista etiquetas de referencia
 		label_ref_list = []
 
-
-		#organiza la lista de etiquetas de referencia para ser comparada
-		for lab_ref in lab_ref_list:	
+		# #organiza la lista de etiquetas de referencia para ser comparada
+		for lab_ref in lab_ref_list:
 			label_ref = lab_ref.upper()
 			label_ref_list.append(label_ref)
-
-
-		count =-1
+		count = 0
+		counter = -1
 		pos = np.zeros(shape=(nch,2))
-		#organiza la lista de etiquetas de la medicion para ser comparada
-		
-		for lab in channels_labels:
 
+		#organiza la lista de etiquetas de la medicion para ser comparada
+		for lab in channels_labels:
 			labe = lab.upper()
 			label1 = labe.find('-')
 			label = labe[0:label1]
 			exist = label in label_ref_list
-			count += 1
+
 			#crea un np.ndarray con las coordenadas de los electrodos existentes en la medida
-			if exist is True:				
-				coord = elec[label]
-				pos[count,:] = coord				
-				coord = np.asarray(coord)
-		counter = -1
+			if exist is True:
+				point = label_ref_list.index(label)
+				coord = elec[point]
+				pos[count,:] = coord
+				# coord = np.asarray(coord)
+			count += 1
+		
 		#elimina los electrodos que no fueron encontrados en la lista de referencia	
 		for coord in pos:
 			counter += 1
 			check = np.array_equal(coord,[0.,0.])
-			if check == True:
+			if check is True:
 				pos = np.delete(pos,counter,0)
 				pot_signal = np.delete(pot_signal,counter,0)
 				counter += -1
-		# print('vector de potencias')
-		# print(pot_signal)
 		# print('posicion de los nodos')
 		# print(pos)
-		return pot_signal, pos
+		return pos,pot_signal,elec
 
 	#elabora el topoplot
 	def topoplot(self,pot_signal,pos):
@@ -89,16 +91,28 @@ class edf_topoplot(object):
 		plt.title('topomap')
 		fig = plot_topomap(pot_signal, pos, cmap='jet', sensors='k.', names=channels_labels, show_names=False,
 				contours=0, image_interp='spline36' ,show = False)
-		plt.savefig(output)
+		# plt.savefig(output)
 		plt.show(fig)
 		return fig
 
+	def mesh(self,pos,elec):
+		# malla de los electrodos de la muestra
+		tri = Delaunay(pos)
+		plt.triplot(pos[:,0], pos[:,1], tri.simplices)
+		plt.plot(pos[:,0], pos[:,1], 'o')
+		plt.show()
+
+		# malla de los electrodos de New York Head Model
+		tri = Delaunay(elec)
+		plt.triplot(elec[:,0], elec[:,1], tri.simplices)
+		plt.plot(elec[:,0], elec[:,1], 'o')
+		plt.show()
+
 if __name__ == '__main__':
 	ob = edf_topoplot()
-	arc,output,configg = ob.argparse()
-	nch,channels_labels, signal = ob.read_edf(arc)
-	pot_signal, pos = ob.cal_topoplot(nch,configg,channels_labels, signal)
+	arc,output,config = ob.argparse()
+	signal,nch,channels_labels = ob.read_edf(arc)
+	pot_signal = ob.calc_power(signal)
+	pos, pot_signal, elec= ob.calc_positions(config,channels_labels,nch,pot_signal)
 	fig = ob.topoplot(pot_signal,pos)
-
-	#python topoplot.py -i sujeto_base.edf -o fig3 -c config
-	
+	_ = ob.mesh(pos,elec)
